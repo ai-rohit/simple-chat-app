fcdd21f8118ea9717e6182c0c9433c621440356e
rohit553-ai <68155913+rohit553-ai@users.noreply.github.com>
2021-12-30 00:27:47 +0545

    added auto scroll feature

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = socketio(server);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const publicDir = path.join(__dirname, "../public");
 
 app.use(express.static(publicDir));
